2e9ed0d49a4b7dfa31bff874623683e0bf31dc8d
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 14:50:11 +0530

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,2 +1,13 @@
 class Card{
+    String rank;
+    String suit;
+
+    public Card(String rank, String suit){
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    public String toString(){
+        return rank + " of " +suit;
+    }
 }
