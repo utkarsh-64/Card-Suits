71743a31ae3856446d0f8f08a59ec652bd751d04
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 15:26:57 +0530

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,13 +1,34 @@
-class Card{
-    String rank;
-    String suit;
+public class Card {
+    private String suit;
+    private String rank;
 
-    public Card(String rank, String suit){
-        this.rank = rank;
+    // This is the Constructor
+    public Card(String suit, String rank) {
         this.suit = suit;
+        this.rank = rank;
     }
 
-    public String toString(){
-        return rank + " of " +suit;
+    // Getters
+    public String getSuit() {
+        return suit;
+    }
+
+    public String getRank() {
+        return rank;
+    }
+
+    // Display card details
+    public void printCard() {
+        System.out.println(rank + " of " + suit);
+    }
+
+    // Compare if two cards have the same suit
+    public boolean sameCard(Card other) {
+        return this.suit.equals(other.suit);
+    }
+
+    // Compare if two cards have the same rank
+    public boolean compareCard(Card other) {
+        return this.rank.equals(other.rank);
     }
 }
