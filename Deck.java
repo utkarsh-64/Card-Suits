17a9ae52a88bd40402353a64b2c9adaf9ace0606
Utkarsh <164793896+utkarsh-64@users.noreply.github.com>
2025-04-29 14:54:38 +0530

    Creating method createDeck

--- Deck.java
+++ Deck.java
@@ -5,4 +5,15 @@ class Deck{
     public Deck(){
         this.deck = new ArrayList<>();
     }
+
+    public void createDeck(){
+        String[] ranks = {"2","3","4","5","6","7","8","9","10","Jack","Queen","King","Ace"};
+        String[] suits = {"Clubs", "Diamonds", "Hearts", "Spades"};
+
+        for (String rank : ranks){
+            for(String suit : suits){
+                deck.add(new Card(rank, suit));
+            }
+        }
+    }
 }
